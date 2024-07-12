92d5c18eff94675eb671f193b80065c6b1462c44
Astralis Lewis <astra.lewis@gmx.com>
2024-07-12 02:43:55 +0100

    PNPM Update

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,6 +14,7 @@ misskey_build() {
     pushd "$install_dir"
         ynh_use_nodejs
         corepack enable
+        corepack prepare pnpm@9.5.0 --activate
         ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production pnpm install --frozen-lockfile
         ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production pnpm run build
         ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production pnpm run init
