e9ef85fd41dabc231aa96c719afc85b5261957e8
Astralis Lewis <astra.lewis@gmx.com>
2024-07-12 02:49:41 +0100

    Testing a change to _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,9 +15,9 @@ misskey_build() {
         ynh_use_nodejs
         corepack enable
         corepack prepare pnpm@9.5.0 --activate
-        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production pnpm install --frozen-lockfile
-        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production pnpm run build
-        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production pnpm run init
+        ynh_exec_warn_less ynh_exec_as "$app" NODE_ENV=production pnpm install --frozen-lockfile
+        ynh_exec_warn_less ynh_exec_as "$app" NODE_ENV=production pnpm run build
+        ynh_exec_warn_less ynh_exec_as "$app" NODE_ENV=production pnpm run init
     popd
 
     ynh_secure_remove --file="$install_dir/.cache"
