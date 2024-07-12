d656daac9876e4e3ab0d9e3ceae774dc9de02eef
Astralis Lewis <astra.lewis@gmx.com>
2024-07-12 02:54:06 +0100

    add CorePack

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,7 +13,7 @@ NODEJS_VERSION="20"
 misskey_build() {
     pushd "$install_dir"
         ynh_use_nodejs
-        #corepack enable
+        corepack enable
         #corepack prepare pnpm@9.5.0 --activate
         ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production pnpm install --frozen-lockfile
         ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production pnpm run build
