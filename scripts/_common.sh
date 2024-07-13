503ddde6f08aa7dd2a83726dae41ee5030103d05
Astralis Lewis <astra.lewis@gmx.com>
2024-07-13 18:46:58 +0100

    Bypassing Git Issue
    Ignoring Dubious Ownership issue

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,6 +14,7 @@ HOME=$install_dir
 misskey_build() {
     pushd "$install_dir"
         git config --system --add safe.directory $install_dir
+        git config --system --add safe.directory $install_dir/*
         ynh_use_nodejs
         git submodule update --init --force
         corepack enable
